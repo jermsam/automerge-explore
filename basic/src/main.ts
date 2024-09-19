c32910b1e5a464e49ee8783056acbaa9b3222b6e
samsonssali <ssali@jitpomi.com>
2024-09-19 05:31:46 -0600

    todolist without automerge

--- basic/src/main.ts
+++ basic/src/main.ts
@@ -1,6 +1,7 @@
 import './style.css'
 import {setup} from './todos.ts';
-// import imagePath from 'assets/bg-desktop-dark.jpg'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div class="bg-[url('assets/bg-mobile-dark.jpg')] md:bg-[url('assets/bg-desktop-dark.jpg')] bg-cover bg-no-repeat h-[30%] ">
@@ -10,18 +11,19 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       <span class="inline-block w-6 h-6 bg-[url('assets/sun.svg')] bg-contain bg-no-repeat"></span>
     </header>
      <main class="relative w-full">
-      <form action="" class="relative todo-form">
+      <div class="relative todo-form">
         <input
+          id="todo-input"
           type="text"
           class="w-full bg-[rgb(37,39,60)] px-12 py-4 rounded-[5px] border-0 focus:text-[hsl(234,39%,85%)]"
           placeholder="Create a new todo..."
         />
-      </form>
+      </div>
        <section class="absolute mt-8 w-full">
-       <ul class="flex flex-col gap-2 bg-[rgb(37,39,60)] rounded"></ul>
+       <ul id="todo-list" class="flex flex-col gap-2 bg-[rgb(37,39,60)] rounded"></ul>
         <div class="flex list-info justify-between items-center bg-[hsl(235,24%,19%)] text-[rgb(119,122,146)] text-[0.8rem] p-4 rounded-[5px]">
           <p><span id="list-length" class="length-num">0</span>items left</p>
-          <button class="clearall-btn">Clear Completed</button>
+          <button id="clear-btn">Clear Completed</button>
         </div>
         <div class="bg-[hsl(235,24%,19%)] flex justify-evenly items-center text-[rgb(119,122,146)] my-1 px-12 py-4 rounded-[5px] border-t-[rgb(119,122,146,0.3)] border-t border-solid">
           <input class="absolute opacity-0 w-0 h-0" type="radio" id="radioAll" name="filter" value="all" />
@@ -45,6 +47,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 `
 
 setup(
-  document.querySelector<HTMLInputElement>('#input')!,
-  document.querySelector<HTMLDivElement>('#list')!
+  document.querySelector<HTMLInputElement>('#todo-input')!,
+  document.querySelector<HTMLLIElement>('#todo-list')!,
+  document.querySelector<HTMLButtonElement>('#clear-btn')!
 )
--- basic/src/todos.ts
+++ basic/src/todos.ts
@@ -1,4 +1,136 @@
+import {TodoItem} from './types';
+import {taskList} from './data.ts';
 
-export function setup(input:HTMLInputElement , list: HTMLDivElement) {
-  console.log(input,list);
+function updateData(data: TodoItem[]=[]) {
+  const tasks = Array.from(document.querySelectorAll("li")).map((liEl) => ({
+    contents: liEl.innerText,
+    completed: liEl.firstElementChild?.classList.contains("completed")||false
+  }));
+
+  data.push(...tasks);
+}
+
+function updateListLength(todoList: HTMLLIElement) {
+  let count = 0;
+  for (let item of todoList.children) {
+    if (!item.firstElementChild?.classList.contains("completed")) {
+      count++;
+    }
+  }
+  const listLength = document.getElementById("list-length") as HTMLElement;
+  listLength.innerText = String(count);
+}
+
+function deleteCompletedTasks(todoList: HTMLLIElement) {
+  const todoItems = Array.from(todoList.children);
+  todoItems.forEach((item) => {
+    if (item.firstElementChild?.classList.contains("completed")) {
+      item.remove();
+    }
+  });
+  updateData(taskList);
+  updateListLength(todoList);
+}
+
+function createTaskItem(task: TodoItem, todoList: HTMLLIElement) {
+  //Create List Item
+  const listItem = document.createElement("li");
+  listItem.classList.add("item");
+
+  //Create Span
+  const styleSpan = document.createElement("span");
+
+  //Add task text to span
+  styleSpan.innerText = task.contents;
+
+  //Append styleSpan to listItem
+  listItem.appendChild(styleSpan);
+  const firstListElement = listItem.firstElementChild as HTMLLIElement;
+
+  if (task.completed) {
+    if(firstListElement) {
+      firstListElement.classList.add("completed");
+    }
+  }
+
+  //Create Completed Button
+  const completedBtn = document.createElement("i");
+  completedBtn.className = "fa-regular fa-circle";
+  completedBtn.addEventListener("click", () => {
+    if(firstListElement) {
+      firstListElement.classList.toggle("completed");
+      if (firstListElement.classList.contains("completed")) {
+        completedBtn.classList.remove("fa-regular", "fa-circle");
+        completedBtn.classList.add("fa-solid", "fa-circle-check");
+      } else {
+        completedBtn.classList.remove("fa-solid", "fa-circle-check");
+        completedBtn.classList.add("fa-regular", "fa-circle");
+      }
+    }
+
+    updateData(taskList);
+    updateListLength(todoList);
+  });
+
+  //Create Delete Button
+  const deleteBtn = document.createElement("i");
+  deleteBtn.className = "fas fa-times";
+  deleteBtn.addEventListener("click", () => {
+    listItem.remove();
+    updateData(taskList);
+    updateListLength(todoList);
+  });
+
+  //Prepend/Append Elements
+  styleSpan.prepend(completedBtn);
+  listItem.appendChild(deleteBtn);
+  todoList.appendChild(listItem);
+}
+
+
+export function setup(input:HTMLInputElement , todoList: HTMLLIElement, clearButton: HTMLButtonElement) {
+  console.log(input,todoList);
+  taskList.forEach(task => createTaskItem(task,todoList))
+  input.addEventListener("keyup", event => {
+    event.preventDefault();
+    if (event.key === 'Enter' || event.code === 'Enter') {
+      // Do something
+      const newTask = input.value;
+      createTaskItem({ contents: newTask, completed: false }, todoList);
+      input.value = "";
+    }
+  })
+  clearButton.addEventListener("click", event => {
+    event.preventDefault();
+    deleteCompletedTasks(todoList)
+  })
+
+  const filterOptions = document.querySelectorAll('input[name=\'filter\']') as unknown as HTMLInputElement[];
+  filterOptions.forEach((input: HTMLInputElement) => {
+    input.addEventListener("change", () => {
+      const filter = input.value;
+      const todoItems = Array.from(todoList.children) as HTMLLIElement[];
+      todoItems.forEach((item) => {
+        switch (filter) {
+          case "all":
+            item.style.display = "flex";
+            break;
+          case "completed":
+            if (item.firstElementChild?.classList.contains("completed")) {
+              item.style.display = "flex";
+            } else {
+              item.style.display = "none";
+            }
+            break;
+          case "active":
+            if (!item.firstElementChild?.classList.contains("completed")) {
+              item.style.display = "flex";
+            } else {
+              item.style.display = "none";
+            }
+            break;
+        }
+      });
+    })
+  })
 }
